ae47e26097b09c6203a3d05ee40dd687f0c96901
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-02-20 09:35:28 +0100

    multiple slices

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import Counter from './components/Counter';
-
+import { Fragment } from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuthanticated = useSelector(
+    (state) => state.authentication.isAuthanticated
+  );
+
+  console.log(isAuthanticated);
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuthanticated && <Auth />}
+      {isAuthanticated && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
